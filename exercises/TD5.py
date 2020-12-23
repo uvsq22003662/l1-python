82061e517f827243077b15689ac4602864d6a5a8
Eryne GUENET <eryne.guenet@ens.uvsq.fr>
2020-12-23 15:34:40 +0100

    td

--- exercises/TD5.py
+++ exercises/TD5.py
@@ -8,6 +8,5 @@ bouton.grid(row = 0, column = 1)
 canvas = tk.Canvas(racine, width = 500, height = 500, background = "black")
 canvas.grid(row = 0, column = 1)
 
-canvas.create_line(0, 0, 500, 500, fill = "yellow")
 
 racine.mainloop()
